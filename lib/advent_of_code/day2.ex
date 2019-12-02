cbe5fcb2b2cd20933d9ef661041f08aa9da96015
Robin Beekhof <robin.beekhof@persgroep.nl>
2019-12-02 18:31:42 +0100

    DAY2 - added calculateOpCode

--- lib/advent_of_code/day2.ex
+++ lib/advent_of_code/day2.ex
@@ -1,4 +1,47 @@
 defmodule AdventOfCode.Day2 do
 
 
+  def retriveIntCode do
+    {_,intCodeString} = File.read("lib/resources/input_dec2.txt")
+    intCodeString
+    |> String.split(",")
+    |> Enum.map(&String.replace(&1,"\n","")|> String.to_integer)
+  end
+
+
+
+  @doc """
+  calculate opcode
+
+  ## Examples
+  iex> intCode = [1,9,10,3,2,3,11,0,99,30,40,50]
+  iex> opCode = [1,9,10,3]
+  iex> {_,[_,_,_,result]} = AdventOfCode.Day2.calculateOpCode(intCode,opCode)
+  iex> result
+  70
+
+  iex> intCode = [1,9,10,70,2,3,11,0,99,30,40,50]
+  iex> opCode = [2,3,11,0]
+  iex> {_,[_,_,_,result]} = AdventOfCode.Day2.calculateOpCode(intCode,opCode)
+  iex> result
+  3500
+
+  iex> intCode = [1,9,10,70,2,3,11,3500,99,30,40,50]
+  iex> opCode = [99,30,40,50]
+  iex> {status,[_,_,_,_]} = AdventOfCode.Day2.calculateOpCode(intCode,opCode)
+  iex> status
+  :stop
+
+
+  """
+  def calculateOpCode(intCode, [pos0,pos1,pos2,pos3]) do
+    case pos0 do
+      1 -> {:ok, [pos0,pos1,pos2,Enum.at(intCode,pos1) + Enum.at(intCode,pos2)]}
+      2 -> {:ok, [pos0,pos1,pos2,Enum.at(intCode,pos1) * Enum.at(intCode,pos2)]}
+      99 -> {:stop, [pos0,pos1,pos2,pos3]}
+      _ -> {:error, [pos0,pos1,pos2,pos3]}
+    end
+  end
+
+
 end
---
+++ test/advent_of_code/day2_test.exs
@@ -0,0 +1,4 @@
+defmodule AdventOfCode.Day2Test do
+  use ExUnit.Case, async: true
+  doctest AdventOfCode.Day2
+end
