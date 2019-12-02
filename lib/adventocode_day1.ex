2ee0b00e4be3ba4d5605faf27d62cc366c0c6166
Robin Beekhof <robin.beekhof@persgroep.nl>
2019-12-02 09:28:13 +0100

    excercise 2 day 1

--- lib/adventocode_day1.ex
+++ lib/adventocode_day1.ex
@@ -4,13 +4,32 @@ defmodule Adventocode.Day1 do
     File.stream!("lib/resources/input_dec1_1.txt")
     |> Enum.to_list
     |> Enum.map(&String.replace(&1,"\n","") |> String.to_integer)
-    |> Enum.map(&(Integer.floor_div(&1,3)-2))
+    |> Enum.map(&fuel_per_weight(&1))
     |> Enum.sum
   end
 
-#  def fuel_for_fuel(fuel, total_fuel) do
-#    con
-#
-#  end
+  def exercise2 do
+    File.stream!("lib/resources/input_dec1_1.txt")
+    |> Enum.to_list
+    |> Enum.map(&String.replace(&1,"\n","") |> String.to_integer)
+    |> Enum.map(&fuel_for_fuel(&1))
+    |> Enum.sum
+  end
 
+  def fuel_for_fuel(fuel_weight) do
+    start_weight = fuel_per_weight(fuel_weight)
+    fuel_for_fuel(start_weight,start_weight)
+  end
+
+  def fuel_for_fuel(fuel_weight, total_fuel_weight) do
+    fuel_weight = fuel_per_weight(fuel_weight)
+    cond do
+      fuel_weight < 0 -> total_fuel_weight
+      true -> fuel_for_fuel(fuel_weight, total_fuel_weight + fuel_weight)
+    end
+  end
+
+  def fuel_per_weight(weight) do
+    Integer.floor_div(weight,3) - 2
+  end
 end
