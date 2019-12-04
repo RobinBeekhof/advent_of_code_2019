064f1aba118d7aefeb6b1f197739ea93a6f2bb29
Robin Beekhof <robin.beekhof@persgroep.nl>
2019-12-04 15:54:34 +0100

    DAY4 - exercise 1

--- lib/advent_of_code/day4.ex
+++ lib/advent_of_code/day4.ex
@@ -1,9 +1,27 @@
 defmodule AdventOfCode.Day4 do
 
+  @doc """
+
+  ## Examples
+      iex> AdventOfCode.Day4.passWordPredicate(111123)
+      true
+
+  """
+
   def passWordPredicate(password) do
-
-
+    digits = password |> Integer.digits
+    
+    digits |> equalOrHigher?
   end
+
+
+  def equalOrHigher?([d1| [d2|_] ]) when d1 >= d2, do: false
+  def equalOrHigher?([_|tail]), do: equalOrHigher?(tail)
+  def equalOrHigher?([]), do: true
+
+  def anyEqualNeighbor?([d1| [d2|_] ]) when d1 == d2, do: true
+  def anyEqualNeighbor?([_|tail]), do: anyEqualNeighbor?(tail)
+  def anyEqualNeighbor?([]), do: false
 
 
 end
